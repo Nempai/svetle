f8f015e8504cb8f649b5163b34441be0430e0707
nempai <mr.honer2007@gmail.com>
2023-06-08 10:27:37 +0300

    Add pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,21 +2,101 @@
     <div class="nav-container">
       <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
       <div class="nav-links">
-        <a href="/about" class="link">Обо мне</a>
-        <a href="/blog" class="link">Блог</a>
-        <a href="/projects" class="link">Портфолио</a>
-        <a href="/contact" class="link">Контакты</a>
+        {#each nav as link}
+        <a href={link.href} class="link">{link.title}</a>
+        {/each}
       </div>
     </div>
   </nav>
   <div class="container">
     <slot />
   </div>
-  
-  <style>
-    .container {
+
+    <style>
+            .container {
       max-width: 1400px;
       margin: 50px auto;
     }
-  </style>
+			body {
+			  background-position: left top;
+			  font-family: "Inter", sans-serif;
+			  margin: 0;
+			  background-repeat: no-repeat;
+			}
+	  
+			* {
+			  box-sizing: border-box;
+			}
+	  
+			a {
+			  text-decoration: none;
+			  color: black;
+			}
+	  
+			nav {
+			  position: sticky;
+			  top: 0;
+			  left: 0;
+			  right: 0;
+			  margin: 0 80px 0 80px;
+			  padding-top: 2rem;
+			  padding-bottom: 2rem;
+			}
+	  
+			nav .nav-container {
+			  display: flex;
+			  justify-content: space-between;
+			  align-items: center;
+			}
+	  
+			.nav-links {
+			  display: flex;
+			  justify-content: flex-end;
+			  align-items: center;
+			  margin: -1em 0;
+			  flex: 1 0 auto;
+			}
+	  
+			.nav-links .link {
+			  position: relative;
+			  margin: 1em;
+			  font-size: 0.85rem;
+			  letter-spacing: 1px;
+			  color: black;
+			}
+	  
+			.nav-logo {
+			  position: relative;
+			  line-height: 1;
+			  font-size: 1.1rem;
+			  color: black;
+			}
+		  </style>
+
+    
+    <script>
+    const nav = [
+    {
+      title: 'Обо мне',
+      href: '/about'
+    },
+    {
+      title: 'Блог',
+      href: '/blog'
+    },
+    {
+      title: 'Портфолио',
+      href: '/project'
+    },
+    {
+      title: 'Контакты',
+      href: '/contact'
+    },
+    {
+      title: 'temp',
+      href: '/temp'
+    }
+  ]
+</script>
+
 
